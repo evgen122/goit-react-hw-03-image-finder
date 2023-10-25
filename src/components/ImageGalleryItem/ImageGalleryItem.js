28db5bcf81f2b45fc61be294810040b36275602e
evgen122 <evg.khristenko@gmail.com>
2023-10-25 09:09:42 +0300

    finish

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,19 +1,11 @@
 import { Component } from 'react';
 import { ItemImg } from './ImageGalleryItem.styled';
-// import * as basicLightbox from 'basiclightbox';
 import { ModalWindow } from 'components/ModalWindow/ModalWindow';
-// import Modal from 'react-modal';
 
 export class ImageGalleryItem extends Component {
   state = {
     isModalState: false,
   };
-
-  // componentDidUpdate(prevState) {
-  //   if (prevState.isModalState !== this.state.isModalState) {
-  //     this.onCloseModal();
-  //   }
-  // }
 
   stateСhangeModal = () => {
     if (this.state.isModalState === true) {
@@ -37,24 +29,6 @@ export class ImageGalleryItem extends Component {
 
     console.log('state', isModalState);
 
-    // Modal.setAppElement('#root');
-
-    // const customStyles = {
-    //   overlay: {
-    //     backgroundColor: '#4e4a48',
-    //     visibility: 'visible',
-    //     visibility: 'hidden',
-    //   },
-    //   content: {
-    //     top: '50%',
-    //     left: '50%',
-    //     right: 'auto',
-    //     bottom: 'auto',
-    //     marginRight: '-50%',
-    //     transform: 'translate(-50%, -50%)',
-    //   },
-    // };
-
     return (
       <li className="gallery-item" key={id} onClick={this.stateСhangeModal}>
         <ItemImg src={webformatURL} alt={tags} />
@@ -66,19 +40,6 @@ export class ImageGalleryItem extends Component {
           src={largeImageURL}
           alt={tags}
         />
-
-        {/* <Modal
-          onClick={isClose}
-          isOpen={isModalState}
-          onRequestClose={this.openModal}
-          shouldCloseOnOverlayClick={false}
-          style={customStyles}
-          contentLabel="onRequestClose"
-        >
-          <img src={largeImageURL} alt={tags} width="800" />
-
-          <button onClick={this.onCloseModal}>Close</button>
-        </Modal> */}
       </li>
     );
   }
--- src/components/ModalWindow/ModalWindow.js
+++ src/components/ModalWindow/ModalWindow.js
@@ -1,59 +1,3 @@
-// import { visible } from 'basiclightbox';
-// import { useState } from 'react';
-// import Button from 'react-bootstrap/Button';
-// import Modal from 'react-bootstrap/Modal';
-
-// export const ModalWindow = ({ isStateModal }) => {
-//   const [show, setShow] = useState(false);
-
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-
-//   return (
-//     <>
-//       <Button variant="primary" onClick={handleShow}>
-//         Launch demo modal
-//       </Button>
-
-//       <Modal show={show} onHide={handleClose}>
-//         <Modal.Header closeButton>
-//           <Modal.Title>Modal heading</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleClose}>
-//             Close
-//           </Button>
-//           <Button variant="primary" onClick={handleClose}>
-//             Save Changes
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-//     </>
-//   );
-// };
-
-// =============================================================
-// import * as basicLightbox from 'basiclightbox';
-
-// export const ModalWindow = ({ isStateModal, isClose, src, alt }) => {
-//   const instance = basicLightbox.create(
-//     `
-//     <img src=${src} width="800" onClice=${isClose}/>
-// `,
-//     { closable: true }
-//   );
-//   if (isStateModal) {
-//     return instance.show();
-//   }
-//   if (isStateModal === false) {
-//     return instance.close();
-//   }
-//   instance.closable();
-// };
-// ===========================================
-
-// ==========================================
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
@@ -61,8 +5,6 @@ Modal.setAppElement('#root');
 const customStyles = {
   overlay: {
     backgroundColor: '#4e4a48',
-    // visibility: 'visible',
-    // visibility: 'hidden',
   },
   content: {
     top: '50%',
@@ -76,7 +18,7 @@ const customStyles = {
 
 export const ModalWindow = ({
   isModalState,
-  isClose,
+  // isClose,
   stateСhangeModal,
   src,
   alt,
@@ -85,7 +27,6 @@ export const ModalWindow = ({
 
   return (
     <Modal
-      // onClick={isClose}
       isOpen={isModalState}
       onRequestClose={stateСhangeModal}
       style={customStyles}
